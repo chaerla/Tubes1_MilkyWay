6e6d2df62cef7dc41efa96b8bce19a60764cd88f
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-02-09 15:59:36 +0700

    recel v1.1

--- src/main/java/Services/BotService.java
+++ src/main/java/Services/BotService.java
@@ -12,7 +12,9 @@ public class BotService {
     private GameState gameState;
     private boolean afterBurnerOn = false;
     private Integer timeSinceLastAction;
-    private GameObject target;
+    private UUID target;
+    private List<GameObject> gameObjects;
+    private List<GameObject> opponents;
 
     public BotService() {
         this.playerAction = new PlayerAction();
@@ -36,58 +38,86 @@ public class BotService {
     }
 
     public void computeNextPlayerAction(PlayerAction playerAction) {
-        List<GameObject> gameObjects = gameState.getGameObjects();
+        gameObjects = gameState.getGameObjects();
+        List<GameObject> players = gameState.getPlayerGameObjects();
+        opponents = new ArrayList<>(players);
+        opponents.removeIf(x -> x.getId() == bot.getId());
+        opponents = opponents.stream().sorted(Comparator
+                .comparing(item -> getDistanceBetween(bot, item)))
+                .collect(Collectors.toList());
+        PlayerAction tempAction = new PlayerAction();
+        tempAction.action = playerAction.action;
+        tempAction.heading = playerAction.heading;
 
-        PlayerAction tempAction = playerAction;
-
-        if (gameObjects.stream().anyMatch(item -> item.getId().equals(bot.getId()))) {
+        if (!(players.stream().anyMatch(item -> item.getId().equals(bot.getId())))) {
             System.out.println("died :(");
+            System.out.println(players);
             return;
         }
 
         if (!gameObjects.isEmpty()) {
-            if (target == null) {
+            var targetObject = gameObjects.stream()
+                    .filter(gameObject -> gameObject.getId() == target)
+                    .findFirst()
+                    .orElse(null);
+            if (targetObject == null){
+                targetObject = opponents.stream()
+                        .filter(gameObject -> gameObject.getId() == target)
+                        .findFirst()
+                        .orElse(null);
+            }
+            if (targetObject == null) {
                 System.out.println("No current target");
                 tempAction = resolveNewTarget();
             } else {
-                if (gameObjects.stream().anyMatch(item -> item.getId().equals(target.getId()))) {
-                    updateTargetInfo();
-                    if (target.getSize() > bot.getSize()) {
-                        tempAction = resolveNewTarget();
-                    } else {
-                        if (playerAction.action == PlayerActions.FIRETORPEDOES) {
-                            if (checkShot(target)) {
-                                tempAction.action = PlayerActions.FORWARD;
-                                tempAction.heading = getHeadingBetween(target);
+//                updateTargetInfo();
+                if (targetObject.getSize() > bot.getSize()) {
+                    tempAction = resolveNewTarget();
+                } else {
+                    if (targetObject.getGameObjectType() == ObjectTypes.PLAYER) {
+                        tempAction.heading = getHeadingBetween(targetObject);
+                        if(playerAction.action == PlayerActions.FORWARD){
+                            if (checkShot(targetObject) && bot.torpedoSalvoCount >=0) {
+                                tempAction.action = PlayerActions.FIRETORPEDOES;
                             } else {
                                 tempAction = resolveNewTarget();
                             }
-                        } else if (playerAction.action == PlayerActions.STARTAFTERBURNER) {
-                            if (checkCollitionWithAcc(target)) {
-                                tempAction.action = PlayerActions.FORWARD;
-                                tempAction.heading = getHeadingBetween(target);
-                            } else {
-                                tempAction.action = PlayerActions.STOPAFTERBURNER;
-                                target = null;
-                            }
-                        } else {
-                            tempAction.heading = getHeadingBetween(target);
-                            System.out.println("current target is a " + target.getGameObjectType());
-                            System.out.println("Going at heading " + tempAction.heading);
                         }
+                        else{
+                            if (checkShot(targetObject) && bot.torpedoSalvoCount >=0) {
+                                tempAction.action = PlayerActions.FORWARD;
+                            } else {
+                                tempAction = resolveNewTarget();
+                            }
+                        }
+                    } else if ((bot.effects & Effects.IsAfterburner.getValue()) == 1) {
+                        if (checkCollitionWithAcc(targetObject)) {
+                            tempAction.action = PlayerActions.FORWARD;
+                            tempAction.heading = getHeadingBetween(targetObject);
+                        } else {
+                            tempAction.action = PlayerActions.STOPAFTERBURNER;
+                            tempAction = resolveNewTarget();
+                        }
+                    } else {
+                        tempAction.heading = getHeadingBetween(targetObject);
                     }
-                } else {
-                    tempAction = resolveNewTarget();
                 }
 
-                while (!checkTarget()) {
+
+                if (!checkTarget(targetObject) && tempAction.action != PlayerActions.FIRETORPEDOES && tempAction.action != PlayerActions.STARTAFTERBURNER) {
+                    System.out.println("resolving target bcs target isnt safe to follow");
                     tempAction = resolveNewTarget();
                 }
             }
 
         }
-
+        System.out.println("===========SENDING COMMAND=================");
         this.playerAction = tempAction;
+//        System.out.println("The selected target is a "+ targetObject.getGameObjectType());
+        System.out.println("The selected heading is "+ this.playerAction.heading);
+        System.out.println("The action is " + this.playerAction.action);
+        System.out.println("============SENDING COMMAND=================");
+
     }
 
     public GameState getGameState() {
@@ -108,15 +138,10 @@ public class BotService {
     // }
 
     private PlayerAction resolveNewTarget() {
-        PlayerAction ret = playerAction;
-        List<GameObject> gameObjects = gameState.getGameObjects();
-        List<GameObject> players = gameState.getPlayerGameObjects();
-        List<GameObject> opponents = players;
-        opponents = players;
-        opponents.removeIf(x -> x.getId() == bot.getId());
-        opponents = opponents.stream().sorted(Comparator
-                .comparing(item -> getDistanceBetween(bot, item)))
-                .collect(Collectors.toList());
+        System.out.println("Resolving new target");
+        PlayerAction ret = new PlayerAction();
+        ret.action = playerAction.action;
+        ret.heading = playerAction.heading;
         List<GameObject> foodList = gameObjects.stream()
                 .filter(item -> item.getGameObjectType() == ObjectTypes.FOOD)
                 .sorted(Comparator
@@ -127,18 +152,18 @@ public class BotService {
             // priority nembak
             if (checkShot(opp)) {
                 System.out.println("Firing torpedoes..");
-                ret.heading = -1 * headingToOpp;
-                ret.action = PlayerActions.FIRETORPEDOES;
-                target = opp;
+                ret.heading = headingToOpp;
+                ret.action = PlayerActions.FORWARD;
+                target = opp.getId();
                 return ret;
             } else if (checkCollitionWithAcc(opp)) {
-                if ((bot.effects & Effects.IsAfterburner.getValue()) == 1) {
+                if ((bot.effects & Effects.IsAfterburner.getValue()) != 1) {
                     System.out.println("Chasing...");
                     ret.action = PlayerActions.STARTAFTERBURNER;
                 } else {
                     ret.action = PlayerActions.FORWARD;
                 }
-                target = opp;
+                target = opp.getId();
                 ret.heading = headingToOpp;
                 return ret;
             }
@@ -149,10 +174,10 @@ public class BotService {
             for (GameObject opponent : opponents) {
                 double distToOpp = getDistanceBetween(bot, opponent);
                 int headingToFood = getHeadingBetween(food);
-                target = food;
-                if (distToFood < distToOpp || checkTarget()
+                if (distToFood < distToOpp || checkTarget(food)
                         || bot.getSize() > opponent.getSize()) {
                     if (!isCrashBorder(food)) {
+                        target = food.getId();
                         System.out.println("Just eating...");
                         ret.action = PlayerActions.FORWARD;
                         ret.heading = headingToFood;
@@ -165,40 +190,46 @@ public class BotService {
         return ret;
     }
 
-    private void updateTargetInfo() {
-        List<GameObject> gameObjects = gameState.getGameObjects();
-        List<GameObject> players = gameState.getPlayerGameObjects();
-        if (target.getGameObjectType() == ObjectTypes.PLAYER) {
-            for (GameObject opp : players) {
-                if (opp.getId() == target.getId()) {
-                    target = opp;
-                }
-            }
-        } else {
-            for (GameObject ob : gameObjects) {
-                if (ob.getId() == target.getId()) {
-                    target = ob;
+//    private void updateTargetInfo() {
+//        System.out.println("=========== UPDATE ==============");
+//        System.out.println("Target is a "+ target.getGameObjectType());
+//        System.out.println("The heading is "+ playerAction.heading);
+//        if (target.getGameObjectType() == ObjectTypes.PLAYER) {
+//            for (GameObject opp : opponents) {
+//                if (opp.getId() == target.getId()) {
+//                    target = opp;
+//                    return;
+//                }
+//            }
+//        } else {
+//            for (GameObject ob : gameObjects) {
+//                if (ob.getId() == target.getId()) {
+//                    target = ob;
+//                    System.out.println("The new heading is "+ getHeadingBetween(target));
+//                    System.out.println("=========== UPDATE ==============");
+//                    return;
+//                }
+//            }
+//        }
+//    }
+
+    private boolean checkTarget(GameObject t) {
+        if(t.getGameObjectType() == ObjectTypes.FOOD){
+            for (GameObject opp : opponents) {
+                if (getDistanceBetween(t, opp) <= (opp.getSize() + bot.getSize())*1.15) {
+                    for(int i = 0;i<100;i++){
+                        System.out.println("AVOIDED HOREeeEe");
+                    }
+                    return false;
                 }
             }
         }
-    }
-
-    private boolean checkTarget() {
-        List<GameObject> players = gameState.getPlayerGameObjects();
-        List<GameObject> opponents = players;
-        opponents = players;
-
-        for (GameObject opp : opponents) {
-            if (getDistanceBetween(target, opp) <= opp.getSize() + bot.getSize()) {
-                return false;
-            }
-        }
-        return !isCrashBorder(target);
+        return !isCrashBorder(t);
     }
 
     private boolean checkShot(GameObject opp) {
         double distToOpp = getDistanceBetween(bot, opp);
-        if (bot.torpedoSalvoCount > 0 && bot.getSize() >= 100) {
+        if (bot.torpedoSalvoCount > 0 && bot.getSize() >= 75) {
             if ((distToOpp + opp.getSize() <= (opp.getSize() / opp.getSpeed() * 60) ||
                     bot.torpedoSalvoCount == 5)) {
                 return true;
@@ -212,19 +243,19 @@ public class BotService {
         return ((bot.speed * 2 > object.speed) && bot.getSize() - tick > object.getSize());
     }
 
-    private boolean isCrash(GameObject object1, GameObject object2) {
-        var triangleX = Math.abs(object1.getPosition().x - object2.getPosition().x);
-        var triangleY = Math.abs(object1.getPosition().y - object2.getPosition().y);
-        var dist = Math.sqrt(triangleX * triangleX + triangleY * triangleY);
-        return (dist + bot.getSize()) >= object2.getSize();
-    }
+    // private boolean isCrash(GameObject object1, GameObject object2) {
+    //     var triangleX = Math.abs(object1.getPosition().x - object2.getPosition().x);
+    //     var triangleY = Math.abs(object1.getPosition().y - object2.getPosition().y);
+    //     var dist = Math.sqrt(triangleX * triangleX + triangleY * triangleY);
+    //     return (dist + bot.getSize()) >= object2.getSize();
+    // }
 
     private boolean isCrashBorder(GameObject object) {
         var worldPos = gameState.getWorld().getCenterPoint();
         var triangleX = Math.abs(worldPos.x - object.getPosition().x);
         var triangleY = Math.abs(worldPos.y - object.getPosition().y);
         var dist = Math.sqrt(triangleX * triangleX + triangleY * triangleY);
-        return (dist + bot.getSize()) * 0.8 >= gameState.getWorld().getRadius();
+        return dist * 0.8 >= gameState.getWorld().getRadius() + bot.getSize();
     }
 
     private void updateSelfState() {
